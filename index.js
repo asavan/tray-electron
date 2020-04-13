b1b89a1631f3d83d810b2ff755d9427d97228117
a.savanovich <asavanster@gmail.com>
2020-04-13 06:02:34 +0300

    make real app

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const { app, Menu, Tray } = require('electron');
 
+const path = require('path');
+const favicon = path.resolve(__dirname, 'favicon.ico');
+const favicon_full = path.resolve(__dirname, 'favicon_full.ico');
+
 let tray = null
 app.on('ready', () => {
-  tray = new Tray('favicon.ico');
+  tray = new Tray(favicon);
   const contextMenu = Menu.buildFromTemplate([
-    { label: 'Item2', type: 'radio', click:  () => {tray.setImage('favicon_full.ico');} },
-    { label: 'Item3', type: 'radio', checked: true, click:  () => {tray.setImage('favicon.ico');}  }
+    { label: 'Item2', type: 'radio', click:  () => {tray.setImage(favicon_full);} },
+    { label: 'Item3', type: 'radio', checked: true, click:  () => {tray.setImage(favicon);}  }
   ])
   tray.setToolTip('Это мое приложение.')
   tray.setContextMenu(contextMenu);
